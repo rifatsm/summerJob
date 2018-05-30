dfad6d0420fc44fee57330951eda65f04624b9fe
Rifat Sabbir Mansur <rifatsm@its-417-2.lib.vt.edu>
2018-05-30 09:24:30 -0400

    top header can now be formatted and written in a new file

---
+++ august-9-1993_out.md
@@ -0,0 +1,5 @@
+---
+layout: strict-rr
+title: University Minutes Archive - August 9, 1993
+crumbtitle: August 9, 1993
+---
--- file_read.py
+++ file_read.py
@@ -1,6 +1,6 @@
 ## Copy the contents of one file to another
 
-# import os
+import re
 
 def scrape_header(tag, line):
 	if tag in line:
@@ -8,18 +8,38 @@ def scrape_header(tag, line):
 		return header[1:-2].replace(": ", " - ")
 
 def scrape_crumbtile(header):
-	print header
+	return header.split(" - ")[1]
 
 
+def write_top_section_in_file(header, crumbtile_date, output_file):
+	bar = "---"
+	layout = "layout: strict-rr"
+	title = "title: " + header
+	crumbtitle = "crumbtitle: " + crumbtile_date
+	with open(output_file, "w") as f_write:
+		f_write.write(bar)
+		f_write.write("\n")
+		f_write.write(layout)
+		f_write.write("\n")
+		f_write.write(title)
+		f_write.write("\n")
+		f_write.write(crumbtitle)
+		f_write.write("\n")
+		f_write.write(bar)
+		f_write.write("\n")
+	return
 
-# Diagnosting
+
+# main 
 
 with open("august-9-1993.md") as f_read:
 	# print f_read.read()
 	tag = "title"
-	print "Header: " + str(scrape_header(tag, f_read.readline()))
-	# for line in f_read:
-	# 	print "Call Func: " + str(scrape_data_inside_tag(tag, line))
+	header = str(scrape_header(tag, f_read.readline()))
+	crumbtile_date = str(scrape_crumbtile(header))
+	output_file = "august-9-1993_out.md"
+	write_top_section_in_file(header, crumbtile_date, output_file)
+	
 
 
 
